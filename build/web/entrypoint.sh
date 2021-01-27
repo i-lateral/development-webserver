e4f052067426e5e43d46c8aa9e26bd731c7ac3ea
Morven Lewis-Everley <morven@i-lateral.com>
2021-01-27 16:55:00 +0000

    Add extra php config options

---
+++ build/web/entrypoint.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Trick to get host.docker.internal working on Linux Docker
+# From https://dev.to/bufferings/access-host-from-a-docker-container-4099
+HOST_DOMAIN="host.docker.internal"
+ping -q -c1 $HOST_DOMAIN > /dev/null 2>&1
+if [ $? -ne 0 ]; then
+  HOST_IP=$(ip route | awk 'NR==1 {print $3}')
+  echo -e "$HOST_IP\t$HOST_DOMAIN" >> /etc/hosts
+fi
+
+# Copy in original PHP docker-php-entrypoint
+set -e
+
+# first arg is `-f` or `--some-option`
+if [ "${1#-}" != "$1" ]; then
+ set -- apache2-foreground "$@"
+fi
+
+exec "$@"
